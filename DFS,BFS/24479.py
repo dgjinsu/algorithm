cd46bd6613516ccf82af3147e16924c94297f0ba
dgjinsu <rlawlstn3206@naver.com>
2023-08-16 16:05:45 +0900

    알고리즘 수업 - 깊이 우선 탐색 1

---
+++ DFS,BFS/24479.py
@@ -0,0 +1,25 @@
+import sys
+
+n, m, r = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+for i in graph:
+    i.sort()
+
+cnt = 1
+def dfs(graph, a, visited):
+    global cnt
+    visited[a] = cnt
+    for i in graph[a]:
+        if visited[i] == 0:
+            cnt += 1
+            dfs(graph, i, visited)
+
+
+dfs(graph, r, visited)
+for i in range(1, n+1):
+    print(visited[i])
