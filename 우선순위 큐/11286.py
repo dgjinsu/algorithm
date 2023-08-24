d6519585756cbc6a923ad9755ffebcf3ac57a796
dgjinsu <rlawlstn3206@naver.com>
2023-08-24 15:42:11 +0900

    절댓값 힙

--- 우선순위 큐/11286.py
+++ 우선순위 큐/11286.py
@@ -11,6 +11,6 @@ for _ in range(n):
         hq.heappush(heap, (abs(x), x)) # 절대값, 원래값 을 넣어 튜플 비교
     else:
         if heap:
-            print(f'정답 {hq.heappop(heap)[1]}')
+            print(hq.heappop(heap)[1])
         else:
-            print("정답: 0")
+            print(0)
