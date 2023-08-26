2856e2248c1c9f88dc2ec3745674509206604b0a
dgjinsu <rlawlstn3206@naver.com>
2023-08-26 18:58:55 +0900

    가장 긴 증가하는 부분 수열 2

--- 이분 탐색/12015.py
+++ 이분 탐색/12015.py
@@ -1,24 +1,35 @@
 import sys
 from collections import deque
+from bisect import bisect_left
+
 input = sys.stdin.readline
 
 n = int(input())
 
-a = list(map(int, input().split(" ")))
+a = list(map(int, input().split()))
 
 lis = []
 lis.append(a[0])
+# for i in a:
+#     left, right = 0, len(lis) - 1
+#     if lis[-1] < i:
+#         lis.append(i)
+#         continue
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if lis[mid] >= i:
+#             right = mid - 1
+#         if lis[mid] < i:
+#             left = mid + 1
+#     lis[left] = i
+
+# print(len(lis))
+
+# bisect 사용
 for i in a:
-    left, right = 0, len(lis) - 1
     if lis[-1] < i:
         lis.append(i)
         continue
-    while left <= right:
-        mid = (left + right) // 2
-        if lis[mid] >= i:
-            right = mid - 1
-        if lis[mid] < i:
-            left = mid + 1
-    lis[left] = i
-
+    index = bisect_left(lis, i)
+    lis[index] = i
 print(len(lis))
