b3aa6b8e60bd32f8f911c5e1efe8546b203e3ba4
dgjinsu <rlawlstn3206@naver.com>
2023-09-05 18:51:22 +0900

    서로 다른 부분 문자열의 개수

---
+++ 집합과 맵/11478.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+s = input().rstrip()
+set_s = set()
+
+for i in range(len(s)):
+  for j in range(i + 1, len(s) + 1):
+    set_s.add(s[i:j])
+
+print(len(set_s))
