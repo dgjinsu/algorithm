bcdbb1cfb9564303c86c4eafa54889c8cbb8bb6d
dgjinsu <rlawlstn3206@naver.com>
2024-06-01 15:35:54 +0900

    programmers

--- 프로그래머스/Lv.1/둘만의 암호.py
+++ 프로그래머스/Lv.1/둘만의 암호.py
@@ -1,19 +1,18 @@
 def solution(s, skip, index):
-    answer = ''
-    skip_set = set()
-    for i in skip:
-        skip_set.add(i)
+    # 알파벳 리스트를 만듭니다. (a ~ z)
+    alphabet = [chr(i) for i in range(97, 123)]
     
-    for i in s:
-        j = index
-        while(j):
-            i = chr(ord(i) + 1)
-            if i in skip_set:
-                continue
-            j -= 1
-        if i > "z":
-            i = chr(ord(i) - 26)
-        answer += i
-
+    # skip에 있는 알파벳을 알파벳 리스트에서 제거합니다.
+    for char in skip:
+        if char in alphabet:
+            alphabet.remove(char)
+    
+    answer = ''
+    
+    # 각 문자에 대해 변환 작업을 수행합니다.
+    for char in s:
+        current_index = alphabet.index(char)
+        new_index = (current_index + index) % len(alphabet)
+        answer += alphabet[new_index]
     
     return answer
