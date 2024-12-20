6632427c5bfb302af97e7f7c769f5f64b8a7e2dd
KIMJINSU <rlawlstn3206@naver.com>
2024-12-20 09:43:06 +0900

    [Bronze V] Title: 개수 세기, Time: 108 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int findNum = Integer.parseInt(bf.readLine());
+        System.out.println(Arrays.stream(arr).filter(x -> x == findNum).count());
+
+    }
+}
