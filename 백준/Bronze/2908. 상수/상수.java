1a84534e1619d25dba5aea5af30873562bfc5721
KIMJINSU <rlawlstn3206@naver.com>
2024-12-20 17:46:02 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -8,21 +8,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        String first = st.nextToken();
-        String second = st.nextToken();
 
-        StringBuilder sbFirst = new StringBuilder();
-        StringBuilder sbSecond = new StringBuilder();
-
-        for (int i = first.length() - 1; i >= 0; i--) {
-            sbFirst.append(first.charAt(i));
-        }
-        for (int i = second.length() - 1; i >= 0; i--) {
-            sbSecond.append(second.charAt(i));
-        }
-
-        int firstInt = Integer.parseInt(sbFirst.toString());
-        int secondInt = Integer.parseInt(sbSecond.toString());
+        int firstInt = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int secondInt = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
 
         System.out.println(Math.max(firstInt, secondInt));
     }
