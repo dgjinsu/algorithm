a24d63b86a4f3d0541612275b11b37ac54183e1e
KIMJINSU <rlawlstn3206@naver.com>
2024-12-27 13:21:42 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1624 ms, Memory: 100364 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static StringTokenizer getSt() throws IOException {
+        return new StringTokenizer(br.readLine());
+    }
+
+    static ArrayList<ArrayList<Integer>> g = new ArrayList<>();
+    static int[] v;
+    static int n;
+    static int m;
+    static int start;
+    static int cnt = 1;
+
+    public static void main(String[] args) throws IOException {
+        st = getSt();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
+        v = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            g.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = getSt();
+            int a  = Integer.parseInt(st.nextToken());
+            int b  = Integer.parseInt(st.nextToken());
+            g.get(a).add(b);
+            g.get(b).add(a);
+        }
+
+        for (int i = 0; i <= n; i++) {
+            Collections.sort(g.get(i));
+        }
+
+        dfs(start);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(v[i]);
+        }
+    }
+
+    static void dfs(int now) {
+        v[now] = cnt;
+        cnt++;
+        for (int node : g.get(now)) {
+            if (v[node] == 0) {
+                dfs(node);
+            }
+        }
+    }
+}
