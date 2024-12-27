aa636e1636bee89110bc752cc5159c466e7dcdf2
KIMJINSU <rlawlstn3206@naver.com>
2024-12-27 13:49:12 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static StringTokenizer getSt() throws IOException {
+        return new StringTokenizer(br.readLine());
+    }
+
+    static ArrayList<ArrayList<Integer>> g = new ArrayList<>();
+    static int[] v;
+    static int n;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        v = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            g.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = getSt();
+            int a  = Integer.parseInt(st.nextToken());
+            int b  = Integer.parseInt(st.nextToken());
+            g.get(a).add(b);
+            g.get(b).add(a);
+        }
+
+        for (int i = 0; i <= n; i++) {
+            Collections.sort(g.get(i));
+        }
+
+        bfs(1);
+
+        System.out.println(Arrays.stream(v).filter(x -> x == 1).count() - 1);
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        v[start]++;
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            for (int next : g.get(now)) {
+                if (v[next] == 0) {
+                    v[next]++;
+                    q.add(next);
+                }
+            }
+        }
+    }
+}
