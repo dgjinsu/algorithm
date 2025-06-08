2b779d88d200a8202df950246beb4e8c7c97d8ba
KIMJINSU <rlawlstn3206@naver.com>
2025-06-08 15:41:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -13,29 +13,25 @@ for i in range(n):
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-def bfs(a, b):
-    global n
-    cnt = 1
-    q = deque()
-    visit[a][b] = 1
-    q.append((a, b))
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n:
-                if homes[nx][ny] == 1 and visit[nx][ny] == 0:
-                    visit[nx][ny] = 1
-                    cnt += 1
-                    q.append((nx, ny))
+cnt = 0
+def dfs(x, y):
+    global cnt, n
+    visit[x][y] = 1
+    cnt += 1
+
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+            if homes[nx][ny] == 1 and visit[nx][ny] == 0:
+                dfs(nx, ny)
     return cnt
 
 result = []
-
 for i in range(n):
     for j in range(n):
         if homes[i][j] == 1 and visit[i][j] == 0:
-            result.append(bfs(i, j))
+            result.append(dfs(i, j))
+            cnt = 0
 
 result.sort()
 print(len(result))
